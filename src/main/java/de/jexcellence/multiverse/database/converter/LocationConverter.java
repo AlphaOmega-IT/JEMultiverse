e292afc9b76ccf8ac6897018957c382a62969bf7
Justin <justin.eiletz@outlook.com>
2025-02-27 21:13:01 +0100

    Important Generators fixes and improvements

--- src/main/java/de/jexcellence/multiverse/database/converter/LocationConverter.java
+++ src/main/java/de/jexcellence/multiverse/database/converter/LocationConverter.java
@@ -10,11 +10,19 @@ public class LocationConverter implements AttributeConverter<Location, String> {
 
   @Override
   public String convertToDatabaseColumn(Location location) {
+    if (
+            location == null
+    ) return null;
+
     return new LocationSerializer().getStringFromLocation(location);
   }
 
   @Override
   public Location convertToEntityAttribute(String s) {
+    if (
+            s == null || s.isEmpty()
+    ) return null;
+
     return new LocationSerializer().getLocationFromString(s);
   }
 }
